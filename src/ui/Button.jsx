4085f1f4641f1bf103e3bdbe1bcfc4449f3937bc
Prince Reyes <krayzerprince@gmail.com>
2023-10-10 22:29:07 +0800

    add styles to menu

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
+function Button({ children, disabled, to, type }) {
   const className =
-    'duratin-300 inline-block rounded-full   bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide  text-stone-800 transition-colors hover:bg-yellow-300   focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed sm:px-6 sm:py-4';
+    'duratin-300 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide  text-stone-800 transition-colors hover:bg-yellow-300   focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed sm:px-6 sm:py-4';
 
+  const base =
+    'duratin-300 inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide  text-stone-800 transition-colors hover:bg-yellow-300   focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+
+  const styles = {
+    primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+  };
   if (to)
     return (
-      <Link className={className} to={to}>
+      <Link className={styles[type]} to={to}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    extend: {
+      colors: {
+        pizza: '#123456',
+      },
+    },
   },
   plugins: [],
 };
