c20c2289877d33e4d977c794640afb7288005ce2
Prince Reyes <krayzerprince@gmail.com>
2023-10-11 13:46:45 +0800

    add additional styles for other components

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,15 +1,14 @@
 import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type }) {
-  const className =
-    'duratin-300 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide  text-stone-800 transition-colors hover:bg-yellow-300   focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed sm:px-6 sm:py-4';
-
   const base =
-    'duratin-300 inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide  text-stone-800 transition-colors hover:bg-yellow-300   focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+    'duratin-300 text-sm inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide  text-stone-800 transition-colors hover:bg-yellow-300   focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
   const styles = {
-    primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'duratin-300 text-sm inline-block px-4 py-2.5 md:px-6 md:py-3.5 rounded-full bg-transparent border-2 border-stone-300 font-semibold uppercase tracking-wide  text-stone-400 transition-colors hover:bg-stone-300 focus:text-stone-300 hover:text-stone-800   focus:bg-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed',
   };
   if (to)
     return (
